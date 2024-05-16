09e3627b51f7e5c934ea2e49c69222d8239b67c3
Szymon Siemieniuk <36564363+siemieniuk@users.noreply.github.com>
2024-05-16 09:39:27 +0200

    Created abstract classes

---
+++ base.py
@@ -0,0 +1,25 @@
+from abc import ABC, abstractmethod
+
+import numpy as np
+
+
+class AbstractNewsClassifier(ABC):
+    def __init__(self, *, metrics: list[str]):
+        self._metrics: list[str] = metrics
+    
+    @abstractmethod
+    def fit(self, x: np.ndarray, y: np.ndarray):
+        pass
+
+    @abstractmethod
+    def predict(self, x: np.ndarray) -> np.ndarray:
+        pass
+
+    @abstractmethod
+    def evaluate(self, x: np.ndarray, y: np.ndarray) -> list[float]:
+        pass
+    
+class AbstractNewsGenerator(ABC): 
+    @abstractmethod
+    def generate(self, title: str) -> str:
+        pass
