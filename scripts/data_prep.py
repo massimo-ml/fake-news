0426763d639780e9ba6280e2ef5463104086e9da
Nikita Makarevich <nikita_makarevich@yahoo.com>
2024-05-30 22:03:52 +0200

    Refactor: better separation of responsibilities in data_prep script; data prep notebook change to use new script

--- scripts/data_prep.py
+++ scripts/data_prep.py
@@ -7,7 +7,7 @@ import argparse
 import logging
 
 
-def clean(df: pd.DataFrame) -> pd.DataFrame:
+def clean_dataset(df: pd.DataFrame) -> pd.DataFrame:
     clean_df = df.copy(deep=True)
     clean_df.loc[clean_df["title"].str.split().str.len() == 0, "title"] = (
         pd.NA
@@ -19,20 +19,16 @@ def clean(df: pd.DataFrame) -> pd.DataFrame:
     return clean_df
 
 
-def preprocess_data(data_dir: str | Path, test_size: float):
-    data_dir = Path(data_dir)
-    logging.info("Reading original data from directory...")
-    data = pd.read_csv(data_dir / "WELFake_Dataset.csv", index_col=0)
-
+def preprocess_dataset(
+    orig_df: pd.DataFrame, test_size: float
+) -> tuple[pd.DataFrame, pd.DataFrame]:
     logging.info("Cleaning data...")
-    clean_df = clean(data)
+    clean_df = clean_dataset(orig_df)
 
     logging.info("Train/test splitting data...")
     train_df, test_df = train_test_split(clean_df, test_size=test_size, random_state=42)
 
-    logging.info("Persisting resulting dataframes...")
-    train_df.to_csv(data_dir / "WELFake_clean_train.csv", index=True)
-    test_df.to_csv(data_dir / "WELFake_clean_test.csv", index=True)
+    return train_df, test_df
 
 
 if __name__ == "__main__":
@@ -52,4 +48,12 @@ if __name__ == "__main__":
     )
     parsed = parser.parse_args()
 
-    preprocess_data(data_dir=parsed.data_dir, test_size=parsed.test_size)
+    data_dir = Path(parsed.data_dir)
+    logging.info("Reading original data from directory...")
+    orig_df = pd.read_csv(data_dir / "WELFake_Dataset.csv", index_col=0)
+
+    train_df, test_df = preprocess_dataset(orig_df=orig_df, test_size=parsed.test_size)
+
+    logging.info("Persisting resulting dataframes...")
+    train_df.to_csv(data_dir / "WELFake_clean_train.csv", index=True)
+    test_df.to_csv(data_dir / "WELFake_clean_test.csv", index=True)
